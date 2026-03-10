2bee645e4ca6783f4e5a13eaf8a6d5a9ce3f79c8
LeoGD-exe <leojanevski123@gmail.com>
2026-03-10 16:10:33 +0100

    Making the template of the house and the camrea llimits working  and removing jump

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,18 +1,13 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 6
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,21 +28,15 @@ func _ready():
 
 var mouse_position: Vector2
 var Camera = Camera3D
-var mouse_rotation_max_x = 1.5
-var mouse_rotation_min_x = -1.5
-var mouse_rotation_now = 0.00
 #so you can move the camera
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouse_position = event.screen_relative
-		rotate_y(-event.screen_relative.x * 0.003)
-		rotate_x(-event.screen_relative.y * 0.003)	
-		
-		mouse_rotation_now += mouse_rotation_now * -0.001
-		mouse_rotation_now = clamp(mouse_rotation_max_x,mouse_rotation_min_x,mouse_rotation_now)
-		
-		
-		
-		
-		
-	
+		rotate_y(-event.relative.x * 0.003)
+		$Camera3D.rotation.x -= event.relative.y * 0.003
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.6, 1.6)
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_tree().quit()
